76bf54af233f47709f24f8887424446360901d2b
Johan Andersson <johan.rejeep@gmail.com>
2001-05-31 15:38:32 +0200

    Added function that checks if a region is selected or not.

--- rejeep/helpers.el
+++ rejeep/helpers.el
@@ -1,1 +1,5 @@
 ;; Contains functions that are used by other functions.
+
+(defun region-selected()
+  "Returns true if a region is selected. False otherwise."
+  (and mark-active transient-mark-mode))
