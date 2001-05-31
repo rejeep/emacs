2fea622f4f4d47b297d402caf5cc5e30398d7b95
Johan Andersson <johan.rejeep@gmail.com>
2001-05-31 16:56:57 +0200

    Added function that kills all buffers except *scratch*.

--- rejeep/buffers.el
+++ rejeep/buffers.el
@@ -1,1 +1,8 @@
 ;; Contains functions related to buffers.
+
+(defun nuke-all-buffers()
+  "Kill all buffers, leaving *scratch* only."
+  (interactive)
+  (mapcar (lambda(x) (kill-buffer x)) (buffer-list))
+  (delete-other-windows)
+  (split-window-horizontally))
