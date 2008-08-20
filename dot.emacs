dcfe960565118fc8956c1ed457300b8b122ae9b7
Johan Andersson <johan.rejeep@gmail.com>
2008-08-20 18:43:29 +0200

    Added load-paths

---
+++ dot.emacs
@@ -0,0 +1,1 @@
+(load "~/.emacs.d/rejeep/loadpath.el")
---
+++ rejeep/loadpath.el
@@ -0,0 +1,3 @@
+(add-to-list 'load-path "~/.emacs.d")
+(add-to-list 'load-path "~/.emacs.d/rejeep")
+(add-to-list 'load-path "~/.emacs.d/packages")
