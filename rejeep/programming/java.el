4e370656fd39cd305411bfe7cacfc7e6b2036d86
Johan Andersson <johan.rejeep@gmail.com>
2008-08-31 15:40:24 +0200

    JDE options

--- rejeep/programming/java.el
+++ rejeep/programming/java.el
@@ -11,4 +11,14 @@
              
              ;; Turn on flymake-mode.
              (flymake-mode 1)
+
+             ;; No "final" when auto creating methods and variables.
+             (setq jde-gen-final-arguments nil)
+             (setq jde-gen-final-methods nil)
+
+             ;; Don't use JDE's builtin abbrevs.
+             (setq jde-enable-abbrev-mode nil)
+
+             ;; So that the left brace is placed on new row.
+             (setq jde-gen-k&r nil)
              ))
