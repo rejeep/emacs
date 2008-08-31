93661d1ec53f6cd1810db623588842b0760cbc9e
Johan Andersson <johan.rejeep@gmail.com>
2008-08-31 13:01:43 +0200

    Changed indention width

--- rejeep/programming/java.el
+++ rejeep/programming/java.el
@@ -1,6 +1,7 @@
 (add-hook 'java-mode-hook
           '(lambda()
-             
+             ;; Two spaces wide indention.
+             (setq c-basic-offset 2)
              ))
 
 (add-hook 'jde-mode-hook
