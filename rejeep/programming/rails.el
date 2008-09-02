4bd2a6cd15dfce5c70ddaa76d02f757db2a23b98
Johan Andersson <johan.rejeep@gmail.com>
2008-09-02 15:17:46 +0200

    Rinari keybindings

--- rejeep/programming/rails.el
+++ rejeep/programming/rails.el
@@ -9,7 +9,21 @@
           '(lambda()
              ;; TAGS file is in rails root if any.
              (setq rinari-tags-file-name "TAGS")
+
+             ;; Rinari find methods.
+             (define-key rinari-minor-mode-map (kbd "C-c c") 'rinari-find-controller)
+             (define-key rinari-minor-mode-map (kbd "C-c m") 'rinari-find-model)
+             (define-key rinari-minor-mode-map (kbd "C-c v") 'rinari-find-view)
+             (define-key rinari-minor-mode-map (kbd "C-c h") 'rinari-find-helper)
+             (define-key rinari-minor-mode-map (kbd "C-c j") 'rinari-find-javascript)
+             (define-key rinari-minor-mode-map (kbd "C-c l") 'rinari-find-lib)
+             (define-key rinari-minor-mode-map (kbd "C-c g") 'rinari-find-migration)
+             (define-key rinari-minor-mode-map (kbd "C-c p") 'rinari-find-public)
+             (define-key rinari-minor-mode-map (kbd "C-c s") 'rinari-find-stylesheet)
+             (define-key rinari-minor-mode-map (kbd "C-c f") 'rinari-find-file-in-project)
+             (define-key rinari-minor-mode-map (kbd "C-c k") 'rinari-find-configuration)
              ))
+
 
 ;; Use rhtml-mode for html.erb files.
 (add-to-list 'auto-mode-alist '("\\.html.erb$" . rhtml-mode))
