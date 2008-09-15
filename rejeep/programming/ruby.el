6a92f14534f090714bdc64cd60cc82671f953982
rejeep <rejeep@xps.(none)>
2008-09-15 16:47:20 +0200

    Small ruby changes

--- rejeep/programming/ruby.el
+++ rejeep/programming/ruby.el
@@ -1,4 +1,6 @@
-(require 'yaml-mode)
-
 ;; Use yaml-mode for yml files.
+(require 'yaml-mode)
 (add-to-list 'auto-mode-alist '("\\.yml$" . yaml-mode))
+
+;; No # -*- coding: utf-8 -*- commets.
+(setq ruby-insert-encoding-magic-comment nil)
