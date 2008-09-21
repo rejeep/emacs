31598cea6336a65665535f5a6f3bf2671d616f59
rejeep <rejeep@xps.(none)>
2008-09-21 19:58:34 +0200

    Created directory for flymake stuff

--- rejeep/programming/ruby.el
+++ rejeep/programming/ruby.el
@@ -3,17 +3,7 @@
 (add-to-list 'auto-mode-alist '("\\.yml$" . yaml-mode))
 
 ;; Flymake.
-(defun flymake-ruby-init()
-  (let* ((temp-file (flymake-init-create-temp-buffer-copy
-                       'flymake-create-temp-inplace))
-         (local-file (file-relative-name
-                       temp-file
-                       (file-name-directory buffer-file-name))))
-    (list "ruby" (list "-c" local-file))))
-
-(push '(".+\\.rb$" flymake-ruby-init) flymake-allowed-file-name-masks)
-(push '("Rakefile$" flymake-ruby-init) flymake-allowed-file-name-masks)
-(push '("^\\(.*\\):\\([0-9]+\\): \\(.*\\)$" 1 2 nil 3) flymake-err-line-patterns)
+(load "rejeep/programming/flymake/ruby.el")
 
 (add-hook 'ruby-mode-hook
           '(lambda()
