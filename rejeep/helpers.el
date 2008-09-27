1f2e993b4ffbdcd5135d05c3ef3a9c226355447b
rejeep <rejeep@xps.(none)>
2008-09-27 16:12:24 +0200

    Added function that moves region in to the specified buffer.

--- rejeep/helpers.el
+++ rejeep/helpers.el
@@ -3,3 +3,12 @@
 (defun region-selected()
   "Returns true if a region is selected. False otherwise."
   (and mark-active transient-mark-mode))
+
+(defun move-region-to-buffer(buffer)
+  (let ((prev-buffer (buffer-name)))    
+    (kill-region (region-beginning) (region-end))
+    (switch-to-buffer buffer)
+    (end-of-buffer)
+    (yank)
+    (insert "\n")
+    (switch-to-buffer prev-buffer)))
