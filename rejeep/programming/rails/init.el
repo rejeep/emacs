1614d21fd84e379528f7faf8bef80018b36c6b8c
rejeep <rejeep@xps.(none)>
2008-09-27 16:13:46 +0200

    Added function to easy create a partial.

--- rejeep/programming/rails/init.el
+++ rejeep/programming/rails/init.el
@@ -11,12 +11,12 @@
 
              ;; Rails find helpers.
              (load "rejeep/programming/rails/find.el")
-             
+
              ;; Keybindings to find methods.
              (define-key rinari-minor-mode-map (kbd "C-c c") 'rinari-find-controller)
              (define-key rinari-minor-mode-map (kbd "C-c m") 'rinari-find-model)
              (define-key rinari-minor-mode-map (kbd "C-c v") 'rinari-find-view)
-             (define-key rinari-minor-mode-map (kbd "C-c h") 'rinari-find-helper)             
+             (define-key rinari-minor-mode-map (kbd "C-c h") 'rinari-find-helper)
              (define-key rinari-minor-mode-map (kbd "C-c g") 'rinari-find-migration)
 
              (define-key rinari-minor-mode-map (kbd "C-c C-c c") 'rails-find-controller)
@@ -35,8 +35,27 @@
 (add-hook 'rhtml-mode-hook
           '(lambda()
              (wrap-region-bind-keys rhtml-mode-map "\"" "'")
+
+             (define-key rhtml-mode-map (kbd "<") 'wrap-region-with-tag-or-insert)
+             (define-key rhtml-mode-map (kbd "C-c p") 'make-partial)
              
-             (define-key rhtml-mode-map (kbd "<") 'wrap-region-with-tag-or-insert)             
+             (defun make-partial(beg end)
+               "Puts region in a partial."
+               (interactive "r")
+               (if (region-selected)
+                   (let ((partial-name) (partial) (prev-buffer (buffer-name)))
+                     (setq partial-name (read-string "Name of partial? "))
+                     (setq partial (concat "_" partial-name ".html.erb"))
+                     (cond ((not (file-exists-p partial))
+                            (move-region-to-buffer partial)
+                            (switch-to-buffer partial)
+                            (create-file-buffer partial)
+                            (write-file partial)
+                            (indent-buffer)
+                            (save-buffer)
+                            (switch-to-buffer prev-buffer)
+                            (insert (concat "<%= render :partial => \"" partial-name "\" %>"))
+                            (indent-for-tab-command))))))
              ))
 
 ;; Use rhtml-mode for html.erb files.
