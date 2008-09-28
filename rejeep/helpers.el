eeb3dfe8c1188211fefed5337dd9ed58fc9ff244
rejeep <rejeep@xps.(none)>
2008-09-28 19:44:43 +0200

    Helper functions added.

--- rejeep/helpers.el
+++ rejeep/helpers.el
@@ -12,3 +12,29 @@
     (yank)
     (insert "\n")
     (switch-to-buffer prev-buffer)))
+
+(defun uniq(list)
+  "Given a list it returns that list but with unique elements."
+  (let ((temp))
+    (dolist (element list)
+      (unless (find element temp)
+        (add-to-list 'temp element)))
+    temp))
+
+(defun keys(alist)
+  "Returns a list of all keys in the alist."
+  (let ((temp))
+    (dolist (element alist)
+      (add-to-list 'temp (car element)))
+    temp))
+
+(defun values(alist)
+  "Returns a list of all values in the alist."
+  (let ((temp))
+    (dolist (element alist)
+      (add-to-list 'temp (cdr element)))
+    temp))
+
+(defun empty(list)
+  "Returns true if list contains no elements. nil otherwise."
+  (= (length list) 0))
