31fee7c07231e86b4eb306858e50004a1fc8a6a0
rejeep <rejeep@xps.(none)>
2008-09-29 20:55:30 +0200

    Truncate long lines

--- rejeep/custom.el
+++ rejeep/custom.el
@@ -2,6 +2,7 @@
  '(jde-compiler (quote (("eclipse java compiler server" "/usr/share/eclipse-ecj-3.3/lib/ecj.jar"))))
  '(flymake-allowed-file-name-masks (quote (("\\.java\\'" jde-ecj-server-flymake-init jde-ecj-flymake-cleanup))))
  '(ecb-options-version "2.32")
+ '(truncate-lines t)
  )
 (custom-set-faces
  '(flymake-errline ((((class color)) (:underline "OrangeRed"))))
