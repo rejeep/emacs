5d31952d5238624c05d5647ca958ce8b0fb66a96
rejeep <rejeep@xps.(none)>
2008-10-17 10:21:30 +0200

    Added function that comments or uncomments whole lines in region or line.

--- rejeep/text.el
+++ rejeep/text.el
@@ -8,3 +8,13 @@
   (backward-word)
   (set-mark (point))
   (forward-word))
+
+(defun comment-or-uncomment-whole-lines-region()
+  "Comments or uncomments whole lines in the selected region
+or on current line if no region is selected."
+  (interactive)
+  (save-excursion
+    (if (region-selected)
+        (mark-whole-lines-region (region-beginning) (region-end))
+      (mark-current-line))
+    (comment-or-uncomment-region (region-beginning) (region-end))))
