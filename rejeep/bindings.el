45d737b0ee6084bc0e215aa14a1c85bbe4b26e85
Johan Andersson <johan.rejeep@gmail.com>
2008-12-25 21:53:13 +0100

    Keybinding updates.

--- rejeep/bindings.el
+++ rejeep/bindings.el
@@ -6,35 +6,23 @@
 ;; Copy region to clippboard.
 (global-set-key (kbd "C-x M-w") 'clipboard-kill-ring-save)
 
-;; Yank from clippboard.
-(global-set-key (kbd "C-x M-v") 'clipboard-yank)
-
 ;; Newline and then indent.
 (global-set-key (kbd "RET") 'reindent-then-newline-and-indent)
 
 ;; Open line below and go to that line.
-(global-set-key (kbd "C-n") 'open-line-below)
+(global-set-key (kbd "M-n") 'open-line-below)
 
 ;; Open line above and go to that line.
-(global-set-key (kbd "C-p") 'open-line-above)
+(global-set-key (kbd "M-p") 'open-line-above)
 
 ;; Toggle ECB show and hide.
 (global-set-key (kbd "§") 'ecb-toggle-ecb-windows)
 
-;; Mark whole buffer.
-(global-set-key (kbd "C-a") 'mark-whole-buffer)
-
 ;; Indent region or buffer.
 (global-set-key (kbd "C-S-f") 'indent-region-or-buffer)
 
-;; Yank.
-(global-set-key (kbd "C-v") 'yank)
-
-;; Yank and pop kill ring.
-(global-set-key (kbd "M-v") 'yank-pop)
-
 ;; Back to indentation or beginning of line.
-(global-set-key (kbd "<home>") 'back-to-indentation-or-beginning-of-line)
+(global-set-key (kbd "C-a") 'back-to-indentation-or-beginning-of-line)
 
 ;; Delete work backwards without saving it to the kill ring.
 (global-set-key (kbd "<M-backspace>") 'backward-delete-word)
@@ -60,30 +48,6 @@
 ;; Magit status.
 (global-set-key (kbd "C-x g") 'magit-status)
 
-;; Move cursor 30 lines down or to end of buffer.
-(global-set-key (kbd "<next>")
-                (lambda ()
-                  (interactive)
-                  (forward-line 30)))
-
-;; Move cursor 30 lines up or to beginning of buffer.
-(global-set-key (kbd "<prior>")
-                (lambda ()
-                  (interactive)
-                  (forward-line -30)))
-
-;; Move five rows up.
-(global-set-key (kbd "<C-up>")
-                (lambda ()
-                  (interactive)
-                  (next-line -5)))
-
-;; Move five rows down.
-(global-set-key (kbd "<C-down>")
-                (lambda ()
-                  (interactive)
-                  (next-line 5)))
-
 ;; So that Emacs never is quit by mistake.
 (global-set-key (kbd "C-x C-c")
                 (lambda ()
@@ -91,24 +55,12 @@
                   (if (y-or-n-p "Quit Emacs? ")
                       (save-buffers-kill-emacs))))
 
-;; Moves line or region five lines up.
-(global-set-key (kbd "<C-M-prior>")
-                (lambda ()
-                  (interactive)
-                  (drag-line-or-region-up 5)))
-
-;; Moves line or region five lines down.
-(global-set-key (kbd "<C-M-next>")
-                (lambda ()
-                  (interactive)
-                  (drag-line-or-region-down 5)))
-
 ;; Perform general cleanup of buffer.
 (global-set-key (kbd "C-c n")
                 (lambda ()
                   (interactive)
-                  (indent-buffer)
+                  (untabify-buffer)
                   (delete-trailing-whitespace)
-                  (untabify-buffer)))
+                  (indent-buffer)))
 
 (provide 'bindings)
