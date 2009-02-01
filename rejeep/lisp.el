f7a3d28b8234bafd65b0d9e3e78c7f6200a4a2c8
Johan Andersson <johan.rejeep@gmail.com>
2009-02-01 10:31:51 +0100

    Don't use pretty lambdas.

--- rejeep/lisp.el
+++ rejeep/lisp.el
@@ -8,16 +8,6 @@
 
              ;; Completion.
              (define-key lisp-mode-shared-map (kbd "C-\\") 'lisp-complete-symbol)
-
-             ;; Replace lambda with lambda.
-             (defun pretty-lambdas ()
-               (font-lock-add-keywords
-                nil `(("(\\(lambda\\>\\)"
-                       (0 (progn (compose-region (match-beginning 1) (match-end 1)
-                                                 ,(make-char 'greek-iso8859-7 107))
-                                 nil))))))
-
-             (pretty-lambdas)
              ))
 
 (provide 'lisp)
