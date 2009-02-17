fb55df1faddafaba00d9b31580bfefd5353dd4ce
Johan Andersson <johan.rejeep@gmail.com>
2009-02-17 22:20:56 +0100

    Flymake for BASH.

--- rejeep/bash.el
+++ rejeep/bash.el
@@ -6,6 +6,10 @@
 
              ;; Indent width is two spaces.
              (setq sh-indentation 2)
+
+             ;; Flymake for shell mode.
+             (require 'flymake-shell)
+             (add-hook 'sh-mode-hook 'flymake-shell-load)
              ))
 
 (provide 'bash)
