e1ecacd8fe50944ff05e814afaa855abad316c12
Johan Andersson <johan.rejeep@gmail.com>
2009-03-06 13:04:18 +0000

    loadpath updates.

--- rejeep/loadpaths.el
+++ rejeep/loadpaths.el
@@ -4,10 +4,9 @@
 (add-to-list 'load-path "~/.emacs.d/packages/yasnippet")
 (add-to-list 'load-path "~/.emacs.d/packages/rhtml")
 (add-to-list 'load-path "~/.emacs.d/packages/yaml-mode")
-(add-to-list 'load-path "~/.emacs.d/packages/mainline")
+(add-to-list 'load-path "~/.emacs.d/packages/magit")
 (add-to-list 'load-path "~/.emacs.d/packages/rinari")
 (add-to-list 'load-path "~/dev/wrap-region")
-(add-to-list 'load-path "~/dev/keats")
 (add-to-list 'load-path "/usr/local/lib/stumpwm/contrib")
 
 (provide 'loadpaths)
