ef324b5a215d768d246ab0c9d8b6551dcbf50b91
Johan Andersson <johan.rejeep@gmail.com>
2009-03-06 13:07:30 +0000

    Use own way to show errors in java code. Other way to slow.

--- rejeep/java.el
+++ rejeep/java.el
@@ -4,11 +4,36 @@
           '(lambda()
              (wrap-region-mode t)
 
+             (defun flymake-java-ecj-init ()
+               (let* ((temp-file   (flymake-init-create-temp-buffer-copy
+                                    'jde-ecj-create-temp-file))
+                      (local-file  (file-relative-name
+                                    temp-file
+                                    (file-name-directory buffer-file-name))))
+                 (list "ecj" (list "-Xemacs" "-d" "/dev/null"
+                                   "-source" "1.5" "-target" "1.5" "-proceedOnError"
+                                   "-classpath"
+                                   (jde-build-classpath jde-global-classpath) local-file))))
+
+             (defun flymake-java-ecj-cleanup ()
+               "Cleanup after `flymake-java-ecj-init' -- delete temp file and dirs."
+               (flymake-safe-delete-file flymake-temp-source-file-name)
+               (when flymake-temp-source-file-name
+                 (flymake-safe-delete-directory (file-name-directory flymake-temp-source-file-name))))
+
+             (defun jde-ecj-create-temp-file (file-name prefix)
+               "Create the file FILE-NAME in a unique directory in the temp directory."
+               (file-truename (expand-file-name (file-name-nondirectory file-name)
+                                                (expand-file-name  (int-to-string (random)) (flymake-get-temp-dir)))))
+
+             (push '(".+\\.java$" flymake-java-ecj-init flymake-java-ecj-cleanup) flymake-allowed-file-name-masks)
+             (push '("\\(.*?\\):\\([0-9]+\\): error: \\(.*?\\)\n" 1 2 nil 2 3 (6 compilation-error-face)) compilation-error-regexp-alist)
+             (push '("\\(.*?\\):\\([0-9]+\\): warning: \\(.*?\\)\n" 1 2 nil 1 3 (6 compilation-warning-face)) compilation-error-regexp-alist)
+
+             (c-set-style "bsd")
+
              ;; Indent width is two spaces.
              (setq c-basic-offset 2)
-
-             ;; Show warnings and errors in code.
-             (require 'jde-eclipse-compiler-server)
 
              ;; Activate flymake.
              (flymake-mode-on)
