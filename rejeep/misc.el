0f7ea100f8b64faf36779f1919aa38a521fb5bd8
Johan Andersson <johan.rejeep@gmail.com>
2009-04-18 19:45:00 +0000

    More utf8 preference.

--- rejeep/misc.el
+++ rejeep/misc.el
@@ -155,8 +155,10 @@
 (require 're-builder)
 
 ;; Prefer utf8.
+(setq locale-coding-system 'utf-8)
 (set-terminal-coding-system 'utf-8)
 (set-keyboard-coding-system 'utf-8)
+(set-selection-coding-system 'utf-8)
 (prefer-coding-system 'utf-8)
 
 ;; Colorize parenthesis.
