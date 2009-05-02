d0843136d7b1edb19165859c5e11958f88aed810
Johan Andersson <johan.rejeep@gmail.com>
2009-05-02 10:37:22 +0000

    Added count-words-region defun.

--- rejeep/defuns.el
+++ rejeep/defuns.el
@@ -117,4 +117,24 @@ current line is commented or uncommented."
         ((looking-back "\\s\)") (backward-list 1))
         (t (self-insert-command arg))))
 
+(defun count-words-region (beginning end)
+  "Print number of words in the region."
+  (interactive "r")
+  (message "Counting words in region ... ")
+  (save-excursion
+    (let ((count 0))
+      (goto-char beginning)
+      (while (and (< (point) end)
+                  (re-search-forward "\\w+\\W*" end t))
+        (setq count (1+ count)))
+      (cond ((zerop count)
+             (message
+              "The region does NOT have any words."))
+            ((= 1 count)
+             (message
+              "The region has 1 word."))
+            (t
+             (message
+              "The region has %d words." count))))))
+
 (provide 'defuns)
