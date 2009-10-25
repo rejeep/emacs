eb5232632fad876520fc0a336f5c3db2484dd976
Johan Andersson <johan.rejeep@gmail.com>
2009-10-25 15:02:33 +0000

    I use js2 these days.

--- rejeep/misc.el
+++ rejeep/misc.el
@@ -18,7 +18,7 @@
 (add-hook 'ruby-mode-hook 'flyspell-prog-mode)
 (add-hook 'emacs-lisp-mode-hook 'flyspell-prog-mode)
 (add-hook 'java-mode-hook 'flyspell-prog-mode)
-(add-hook 'espresso-mode-hook 'flyspell-prog-mode)
+(add-hook 'js2-mode-hook 'flyspell-prog-mode)
 (add-hook 'magit-log-edit-mode-hook 'flyspell-mode)
 (add-hook 'yaml-mode-hook 'flyspell-mode)
 
