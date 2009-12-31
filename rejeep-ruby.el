976507b1ae511fc8ca9c68a5c805125ea44dede5
Johan Andersson <johan.rejeep@gmail.com>
2009-12-31 12:38:54 +0000

    Organize ruby stuff better.

--- rejeep-ruby.el
+++ rejeep-ruby.el
@@ -1,103 +1,34 @@
 ;;; rejeep-ruby.el --- Ruby and Rails specific settings
 
-(require 'rinari)
+(add-to-list 'auto-mode-alist '("\\.rake$" . ruby-mode))
+(add-to-list 'auto-mode-alist '("Rakefile$" . ruby-mode))
+(add-to-list 'auto-mode-alist '("\\.gemspec$" . ruby-mode))
 
+;; Ruby
 (eval-after-load 'ruby-mode
   '(progn
      (wrap-region-mode t)
 
      (define-key ruby-mode-map (kbd "C-M-h") 'backward-kill-word)))
 
-;; Flymake
-(eval-after-load 'ruby-mode
-  '(progn
-     ;; Invoke ruby with '-c' to get syntax checking
-     (defun flymake-ruby-init ()
-       (let* ((temp-file (flymake-init-create-temp-buffer-copy
-                          'flymake-create-temp-inplace))
-              (local-file (file-relative-name
-                           temp-file
-                           (file-name-directory buffer-file-name))))
-         (list "ruby" (list "-c" local-file))))
-
-     (push '(".+\\.rb$" flymake-ruby-init) flymake-allowed-file-name-masks)
-     (push '("Rakefile$" flymake-ruby-init) flymake-allowed-file-name-masks)
-
-     (push '("^\\(.*\\):\\([0-9]+\\): \\(.*\\)$" 1 2 nil 3)
-           flymake-err-line-patterns)
-
-     (add-hook 'ruby-mode-hook
-               (lambda ()
-                 (when (and buffer-file-name
-                            (file-writable-p
-                             (file-name-directory buffer-file-name))
-                            (file-writable-p buffer-file-name)
-                            (if (fboundp 'tramp-list-remote-buffers)
-                                (not (subsetp
-                                      (list (current-buffer))
-                                      (tramp-list-remote-buffers)))
-                              t))
-                   (local-set-key (kbd "C-c d") 'flymake-display-err-menu-for-current-line)
-                   (flymake-mode t))))))
-
-
-
-
-
-
-
-
-
-
-(autoload 'rhtml-mode "rhtml-mode" "" t)
-
-(eval-after-load 'ruby-mode
-  '(progn
-     (require 'rinari)
-     (require 'flymake)
-
-     ;; Invoke ruby with '-c' to get syntax checking
-     (defun flymake-ruby-init ()
-       (let* ((temp-file (flymake-init-create-temp-buffer-copy
-                          'flymake-create-temp-inplace))
-              (local-file (file-relative-name
-                           temp-file
-                           (file-name-directory buffer-file-name))))
-         (list "ruby" (list "-c" local-file))))
-
-     (push '(".+\\.rb$" flymake-ruby-init) flymake-allowed-file-name-masks)
-     (push '("Rakefile$" flymake-ruby-init) flymake-allowed-file-name-masks)
-
-     (push '("^\\(.*\\):\\([0-9]+\\): \\(.*\\)$" 1 2 nil 3)
-           flymake-err-line-patterns)
-
-     (add-hook 'ruby-mode-hook
-               (lambda ()
-                 (when (and buffer-file-name
-                            (file-writable-p
-                             (file-name-directory buffer-file-name))
-                            (file-writable-p buffer-file-name)
-                            (not (subsetp
-                                  (list (current-buffer))
-                                  (tramp-list-remote-buffers))))
-                   (local-set-key (kbd "C-c d")
-                                  'flymake-display-err-menu-for-current-line)
-                   (flymake-mode t))))))
-
+;; Rinari
+(require 'rinari)
 (add-hook 'rinari-minor-mode-hook
           '(lambda()
              ;; No # -*- coding: utf-8 -*- commets.
              (setq ruby-insert-encoding-magic-comment nil)
 
              ;; TAGS file is in rails root if any.
-             (setq rinari-tags-file-name "TAGS")
-             ))
+             (setq rinari-tags-file-name "TAGS")))
 
+;; Rhtml
+(autoload 'rhtml-mode "rhtml-mode" "" t)
+(add-to-list 'auto-mode-alist '("\\.rhtml$" . rhtml-mode))
+(add-to-list 'auto-mode-alist '("\\.html.erb$" . rhtml-mode))
 (add-hook 'rhtml-mode-hook
           '(lambda()
              ;; < should insert tag.
              (setq wrap-region-tag-active t)
-             (wrap-region-mode t)
-             ))
+             (wrap-region-mode t)))
 
 (provide 'rejeep-ruby)
