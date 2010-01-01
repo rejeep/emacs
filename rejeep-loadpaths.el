07d247df1ad4d847e183340d40d3e8276fee11ee
Johan Andersson <johan.rejeep@gmail.com>
2010-01-01 18:33:05 +0000

    Added color-theme package.

--- rejeep-loadpaths.el
+++ rejeep-loadpaths.el
@@ -1,12 +1,13 @@
 ;;; loadpaths.el --- Load paths
 
 (add-to-list 'load-path "~/.emacs.d/packages")
-(add-to-list 'load-path "~/.emacs.d/packages/yasnippet")
-(add-to-list 'load-path "~/.emacs.d/packages/rhtml")
+(add-to-list 'load-path "~/.emacs.d/packages/color-theme")
 (add-to-list 'load-path "~/.emacs.d/packages/cucumber")
-(add-to-list 'load-path "~/.emacs.d/packages/markdown-mode")
 (add-to-list 'load-path "~/.emacs.d/packages/keats-mode")
+(add-to-list 'load-path "~/.emacs.d/packages/markdown-mode")
+(add-to-list 'load-path "~/.emacs.d/packages/rhtml")
 (add-to-list 'load-path "~/.emacs.d/packages/wrap-region-mode")
+(add-to-list 'load-path "~/.emacs.d/packages/yasnippet")
 (add-to-list 'load-path "~/dev/rinari")
 
 (provide 'rejeep-loadpaths)
