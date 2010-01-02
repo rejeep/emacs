1bad7f93318d3c9363325c1093fc68bd41a0a557
Johan Andersson <johan.rejeep@gmail.com>
2010-01-02 14:18:12 +0000

    Turn on Eldoc mode in Emacs Lisp mode.

--- rejeep-lisp.el
+++ rejeep-lisp.el
@@ -1,9 +1,11 @@
 ;;; rejeep-lisp.el --- All that has to do with (any dialect of) Lisp
 
+(add-hook 'emacs-lisp-mode-hook 'turn-on-eldoc-mode)
+
 (add-hook 'emacs-lisp-mode-hook
           '(lambda ()
              (wrap-region-mode t)
-
+             
              ;; Completion.
              (define-key lisp-mode-shared-map (kbd "C-\\") 'lisp-complete-symbol)))
 
