4cda4d0ec7a7daf0120dff4386268e4c807f1513
Johan Andersson <johan.rejeep@gmail.com>
2010-01-02 19:26:10 +0000

    Fixing some rhtml colors.

--- rejeep-ruby.el
+++ rejeep-ruby.el
@@ -25,9 +25,17 @@
 (autoload 'rhtml-mode "rhtml-mode" "" t)
 (add-to-list 'auto-mode-alist '("\\.rhtml$" . rhtml-mode))
 (add-to-list 'auto-mode-alist '("\\.html.erb$" . rhtml-mode))
+
 (add-hook 'rhtml-mode-hook
           '(lambda()
-             ;; < should insert tag.
+             (custom-set-faces
+              '(erb-comment-delim-face ((t (:foreground "#5F5A60" :background "grey15"))))
+              '(erb-comment-face ((t (:italic t :foreground "#5F5A60" :background "grey15"))))
+              '(erb-delim-face ((t (:foreground "#FFAEEA" :background "grey15"))))
+              '(erb-face ((t (:background "grey15"))))
+              '(erb-out-delim-face ((t (:foreground "#FFAEEA" :background "grey15"))))
+              '(erb-out-face ((t (:background "grey15")))))
+
              (setq wrap-region-tag-active t)
              (wrap-region-mode t)))
 
