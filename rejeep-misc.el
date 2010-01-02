289e023c7ee12612200cb2ee640c32323c6c0718
Johan Andersson <johan.rejeep@gmail.com>
2010-01-02 14:48:48 +0000

    Fix comment.

--- rejeep-misc.el
+++ rejeep-misc.el
@@ -95,8 +95,7 @@
 ;; delete-trailing-whitespace is to long.
 (defalias 'dtw 'delete-trailing-whitespace)
 
-;; Add parts of each file's directory to the buffer name if not
-;; unique.
+;; Add parts of each file's directory to the buffer name if not unique.
 (require 'uniquify)
 (setq uniquify-buffer-name-style 'forward)
 
