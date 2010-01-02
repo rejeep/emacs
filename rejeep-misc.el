2bf25f9c4213d284fe3335756bedcd35d75c4127
Johan Andersson <johan.rejeep@gmail.com>
2010-01-02 21:26:43 +0000

    Keats file is in .emacs.d

--- rejeep-misc.el
+++ rejeep-misc.el
@@ -87,7 +87,7 @@
 (setq-default c-basic-offset 2)
 
 ;; Use keats to handle keyboard shortcuts.
-(setq keats-file "~/dev/emacs/keats")
+(setq keats-file "~/emacs.d/keats")
 (require 'keats)
 (require 'keats-interactive)
 (keats-mode t)
