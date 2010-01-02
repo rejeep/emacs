70b27b734602aac35e4e28300b7dbc3a179440d5
Johan Andersson <johan.rejeep@gmail.com>
2010-01-02 22:08:32 +0000

    This is not flymake, but flyspell.

--- rejeep-flymake.el
+++ rejeep-flymake.el
@@ -2,17 +2,6 @@
 
 (require 'flymake)
 
-;; Default spelling dictionary.
-(setq ispell-dictionary "english")
-
-;; Add flyspell to modes.
-(add-hook 'ruby-mode-hook 'flyspell-prog-mode)
-(add-hook 'emacs-lisp-mode-hook 'flyspell-prog-mode)
-(add-hook 'java-mode-hook 'flyspell-prog-mode)
-(add-hook 'js2-mode-hook 'flyspell-prog-mode)
-(add-hook 'magit-log-edit-mode-hook 'flyspell-mode)
-(add-hook 'yaml-mode-hook 'flyspell-mode)
-
 ;; Ruby
 (eval-after-load 'ruby-mode
   '(progn
