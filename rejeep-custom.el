8a3f05947be01451493bb6c5a46305c4b6925bd4
Johan Andersson <johan.rejeep@gmail.com>
2010-01-03 21:38:33 +0000

    Use default colors.

--- rejeep-custom.el
+++ rejeep-custom.el
@@ -7,10 +7,3 @@
   ;; If there is more than one, they won't work right.
  '(truncate-lines t)
  '(yas/prompt-functions (quote (yas/dropdown-prompt yas/ido-prompt yas/x-prompt yas/completing-prompt yas/no-prompt))))
-(custom-set-faces
-  ;; custom-set-faces was added by Custom.
-  ;; If you edit it by hand, you could mess it up, so be careful.
-  ;; Your init file should contain only one such instance.
-  ;; If there is more than one, they won't work right.
- '(erb-delim-face ((t (:foreground "#FFAEEA"))))
- '(erb-out-delim-face ((((background dark)) (:foreground "#FFAEEA")))))
