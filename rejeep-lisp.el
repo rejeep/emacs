2c1c56cef909b2107c249807914959ea6f5f8dcc
Johan Andersson <johan.rejeep@gmail.com>
2010-01-07 19:39:42 +0000

    Don't need to be in a hook.

--- rejeep-lisp.el
+++ rejeep-lisp.el
@@ -3,8 +3,7 @@
 (add-hook 'emacs-lisp-mode-hook 'turn-on-eldoc-mode)
 (add-hook 'emacs-lisp-mode-hook 'flyspell-prog-mode)
 (add-hook 'emacs-lisp-mode-hook 'wrap-region-mode)
-(add-hook 'emacs-lisp-mode-hook
-          '(lambda ()
-             (define-key lisp-mode-shared-map (kbd "C-\\") 'lisp-complete-symbol)))
+
+(define-key lisp-mode-shared-map (kbd "C-\\") 'lisp-complete-symbol)
 
 (provide 'rejeep-lisp)
