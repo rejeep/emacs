e88db18d1aa8b65a70cad804bf46695aebf7481e
Johan Andersson <johan.rejeep@gmail.com>
2010-01-22 12:58:01 +0000

    Markdown files can have "md" as extension.

--- rejeep-programming.el
+++ rejeep-programming.el
@@ -5,6 +5,7 @@
 ;; Markdown
 (autoload 'markdown-mode "markdown-mode.el" "Major mode for editing Markdown files" t)
 (add-to-list 'auto-mode-alist '("\.markdown$" . markdown-mode))
+(add-to-list 'auto-mode-alist '("\.md$" . markdown-mode))
 (add-hook 'markdown-mode-hook
           '(lambda()
              (wrap-region-set-mode-punctuations '("[" "("))
