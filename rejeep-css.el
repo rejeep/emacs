d126dcb814950dfdfcf2a9baa2ef6cf16c115daf
Johan Andersson <johan.rejeep@gmail.com>
2010-01-24 16:52:23 +0000

    Show colors, em & px and url with different colors.

--- rejeep-css.el
+++ rejeep-css.el
@@ -5,6 +5,10 @@
 
 (eval-after-load 'css-mode
   '(progn
-     (setq css-indent-offset 2)))
+     (setq css-indent-offset 2)
+     (font-lock-add-keywords 'css-mode
+                             '(("#[a-fA-F0-9][a-fA-F0-9][a-fA-F0-9]\\([a-fA-F0-9][a-fA-F0-9][a-fA-F0-9]\\)?" . font-lock-reference-face)
+                               ("[0-9]+\\(px\\|em\\)" 1 font-lock-keyword-face)
+                               ("\\(url\\)(" 1 font-lock-function-name-face)))))
 
 (provide 'rejeep-css)
