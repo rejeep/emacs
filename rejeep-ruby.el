927ccd821198978693192d7fb0aef2c651e82568
Johan Andersson <johan.rejeep@gmail.com>
2010-02-13 12:08:49 +0000

    Gemfile wants ruby-mode.

--- rejeep-ruby.el
+++ rejeep-ruby.el
@@ -3,6 +3,7 @@
 (add-to-list 'auto-mode-alist '("\\.rake$" . ruby-mode))
 (add-to-list 'auto-mode-alist '("Rakefile$" . ruby-mode))
 (add-to-list 'auto-mode-alist '("\\.gemspec$" . ruby-mode))
+(add-to-list 'auto-mode-alist '("Gemfile$" . ruby-mode))
 
 (add-hook 'ruby-mode-hook 'flyspell-prog-mode)
 (add-hook 'ruby-mode-hook 'wrap-region-mode)
