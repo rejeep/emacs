5abaf6a1c5456686e4c8b7cf4b4be4c35a9728d2
Johan Andersson <johan.rejeep@gmail.com>
2010-02-14 11:40:22 +0000

    Set some nice colors for diff-mode.

--- rejeep-custom.el
+++ rejeep-custom.el
@@ -7,3 +7,11 @@
   ;; If there is more than one, they won't work right.
  '(truncate-lines t)
  '(yas/prompt-functions (quote (yas/dropdown-prompt yas/ido-prompt yas/x-prompt yas/completing-prompt yas/no-prompt))))
+(custom-set-faces
+  ;; custom-set-faces was added by Custom.
+  ;; If you edit it by hand, you could mess it up, so be careful.
+  ;; Your init file should contain only one such instance.
+  ;; If there is more than one, they won't work right.
+ '(diff-added ((t (:inherit diff-changed :foreground "green"))))
+ '(diff-changed ((nil (:foreground "yellow"))))
+ '(diff-removed ((t (:inherit diff-changed :foreground "red")))))
