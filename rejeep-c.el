6a994cce1b239ececa83c1ed4840c597ae580035
Johan Andersson <johan.rejeep@gmail.com>
2010-02-14 11:39:53 +0000

    Add file for c programming.

---
+++ rejeep-c.el
@@ -0,0 +1,7 @@
+;;; rejeep-c.el --- c programming stuff
+
+(add-hook 'c-mode-hook
+          (lambda()
+            (c-set-style "K&R")
+            (setq tab-width 2)
+            (setq c-basic-offset 2)))
