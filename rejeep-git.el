2cf438fd9c9d4e07e39797e20ac52580a8750f31
Johan Andersson <johan.rejeep@gmail.com>
2010-03-30 23:14:43 +0000

    Not working any good.

--- rejeep-git.el
+++ rejeep-git.el
@@ -5,15 +5,6 @@
 ;; Correct spelling on commit messages.
 (add-hook 'magit-log-edit-mode-hook 'flyspell-mode)
 
-;; These are the directories in which I want Magit to look for Git repos.
-(setq magit-repo-dirs '("~/dev"))
-
-;; Only look one level down for repos.
-(setq magit-repo-dirs-depth 1)
-
-;; Use Ido for selecting repos.
-(setq magit-completing-read 'ido-completing-read)
-
 ;; Set diff colors.
 (add-hook 'magit-mode-hook
           (lambda()
