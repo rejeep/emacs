f54f27553e02eb122a6e7ecf374c89dc3c9e1355
Johan Andersson <johan.rejeep@gmail.com>
2010-05-07 08:47:10 +0000

    Don't need this since using builtin css-mode.

--- rejeep-css.el
+++ rejeep-css.el
@@ -1,8 +1,5 @@
 ;;; rejeep-css.el --- CSS specific settings.
 
-(autoload 'css-mode "css-mode")
-(setq auto-mode-alist (cons '("\\.css\\'" . css-mode) auto-mode-alist))
-
 (eval-after-load 'css-mode
   '(progn
      (setq css-indent-offset 2)
