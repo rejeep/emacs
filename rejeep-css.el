05ee2e55548827f25182bb67d63b60f331f2a68b
Johan Andersson <johan.rejeep@gmail.com>
2010-05-07 08:47:51 +0000

    Do not quote.

--- rejeep-css.el
+++ rejeep-css.el
@@ -1,11 +1,11 @@
 ;;; rejeep-css.el --- CSS specific settings.
 
 (eval-after-load 'css-mode
-  '(progn
-     (setq css-indent-offset 2)
-     (font-lock-add-keywords 'css-mode
-                             '(("#[a-fA-F0-9][a-fA-F0-9][a-fA-F0-9]\\([a-fA-F0-9][a-fA-F0-9][a-fA-F0-9]\\)?" . font-lock-reference-face)
-                               ("[0-9]+\\(px\\|em\\|%\\)" 1 font-lock-keyword-face)
-                               ("\\(url\\)(" 1 font-lock-function-name-face)))))
+  (progn
+    (setq css-indent-offset 2)
+    (font-lock-add-keywords 'css-mode
+                            '(("#[a-fA-F0-9][a-fA-F0-9][a-fA-F0-9]\\([a-fA-F0-9][a-fA-F0-9][a-fA-F0-9]\\)?" . font-lock-reference-face)
+                              ("[0-9]+\\(px\\|em\\|%\\)" 1 font-lock-keyword-face)
+                              ("\\(url\\)(" 1 font-lock-function-name-face)))))
 
 (provide 'rejeep-css)
