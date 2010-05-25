fc2cc515861fea599a08a01ec92db2cd8f394f84
Johan Andersson <johan.rejeep@gmail.com>
2010-05-25 21:06:27 +0200

    Make Lisp complete symbol OSX friendly.

--- rejeep-lisp.el
+++ rejeep-lisp.el
@@ -3,7 +3,7 @@
 (add-hook 'emacs-lisp-mode-hook 'turn-on-eldoc-mode)
 (add-hook 'emacs-lisp-mode-hook 'flyspell-prog-mode)
 
-(define-key lisp-mode-shared-map (kbd "C-\\") 'lisp-complete-symbol)
+(define-key lisp-mode-shared-map (kbd "C-9") 'lisp-complete-symbol)
 
 (add-to-list 'interpreter-mode-alist '("emacs" . emacs-lisp-mode))
 
