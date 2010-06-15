ad851f9cfbee380869e6c297cc108a4910b7d1c8
Johan Andersson <johan.rejeep@gmail.com>
2010-06-15 14:58:21 +0200

    Move OSX load to the bottom and separate sections with a comment.

--- init.el
+++ init.el
@@ -1,14 +1,14 @@
 (add-to-list 'load-path "~/.emacs.d")
 
+;; Misc
 (require 'rejeep-paths)
-
 (require 'rejeep-elpa)
 (require 'rejeep-ido)
 (require 'rejeep-defuns)
 (require 'rejeep-bindings)
 (require 'rejeep-misc)
-(require 'rejeep-osx)
 
+;; Programming
 (require 'rejeep-programming)
 (require 'rejeep-java)
 (require 'rejeep-ruby)
@@ -20,3 +20,7 @@
 (require 'rejeep-cucumber)
 (require 'rejeep-c)
 (require 'rejeep-git)
+
+;; Others
+(require 'rejeep-osx)
+
