091f18fdbc10a3d06a0ecbf973da1bebb93eec8a
Johan Andersson <johan.rejeep@gmail.com>
2010-06-28 13:21:48 +0200

    Remove some load-path's.

--- rejeep-paths.el
+++ rejeep-paths.el
@@ -3,16 +3,11 @@
 (add-to-list 'load-path "~/.emacs.d/packages")
 (add-to-list 'load-path "~/.emacs.d/packages/color-theme")
 (add-to-list 'load-path "~/.emacs.d/packages/cucumber")
-(add-to-list 'load-path "~/.emacs.d/packages/drag-stuff")
-(add-to-list 'load-path "~/.emacs.d/packages/keats-mode")
-(add-to-list 'load-path "~/.emacs.d/packages/magit")
 (add-to-list 'load-path "~/.emacs.d/packages/markdown-mode")
 (add-to-list 'load-path "~/.emacs.d/packages/rhtml")
 (add-to-list 'load-path "~/.emacs.d/packages/rinari")
-(add-to-list 'load-path "~/.emacs.d/packages/wrap-region")
 (add-to-list 'load-path "~/.emacs.d/packages/yasnippet")
 (add-to-list 'load-path "~/.emacs.d/packages/color-theme-rejeep")
-(add-to-list 'load-path "~/.emacs.d/packages/html-script-src")
 
 (add-to-list 'exec-path "/usr/local/bin")
 
