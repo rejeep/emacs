78fabdc5247b7033e9674b0ca6a70c41942a9190
Johan Andersson <johan.rejeep@gmail.com>
2010-11-10 08:58:23 +0100

    Added function for string interpolation.

--- rejeep-ruby.el
+++ rejeep-ruby.el
@@ -1,12 +1,23 @@
 ;;; rejeep-ruby.el --- Ruby specific settings
 
 
+(defun ruby-interpolate ()
+  "In in a double quoted string, interpolate."
+  (interactive)
+  (insert "#")
+  (let ((properties (text-properties-at (point))))
+    (when (and
+           (memq 'font-lock-string-face properties)
+           (save-excursion
+             (ruby-forward-string "\"" (line-end-position) t)))
+      (insert "{}")
+      (backward-char 1))))
 
-;; Don't mess with my bindings.
 (add-hook 'ruby-mode-hook
           (lambda()
             (define-key ruby-mode-map (kbd "C-M-n") 'scroll-up-five)
-            (define-key ruby-mode-map (kbd "C-M-p") 'scroll-down-five)))
+            (define-key ruby-mode-map (kbd "C-M-p") 'scroll-down-five)
+            (define-key ruby-mode-map (kbd "#") 'ruby-interpolate)))
 
 (dolist (regex '("\\.rake$" "\\.gemspec$" "\\.ru$" "Rakefile$" "Gemfile$" "Capfile$"))
   (add-to-list 'auto-mode-alist `(,regex . ruby-mode)))
