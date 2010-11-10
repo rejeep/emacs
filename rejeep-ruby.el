2730a9706ff26dae315c1cae94fa6fd257d899ea
Johan Andersson <johan.rejeep@gmail.com>
2010-11-10 08:58:11 +0100

    Remove ruby flymake stuff.

--- rejeep-ruby.el
+++ rejeep-ruby.el
@@ -8,38 +8,6 @@
             (define-key ruby-mode-map (kbd "C-M-n") 'scroll-up-five)
             (define-key ruby-mode-map (kbd "C-M-p") 'scroll-down-five)))
 
-
-;;; Flymake
-
-(defun flymake-ruby-init ()
-  (let* ((temp-file (flymake-init-create-temp-buffer-copy
-                     'flymake-create-temp-inplace))
-         (local-file (file-relative-name
-                      temp-file
-                      (file-name-directory buffer-file-name))))
-    ;; Invoke ruby with '-c' to get syntax checking
-    (list "ruby" (list "-c" local-file))))
-
-(defun flymake-ruby-enable ()
-  (when (and buffer-file-name
-             (file-writable-p
-              (file-name-directory buffer-file-name))
-             (file-writable-p buffer-file-name)
-             (if (fboundp 'tramp-list-remote-buffers)
-                 (not (subsetp
-                       (list (current-buffer))
-                       (tramp-list-remote-buffers)))
-               t))
-    (flymake-mode t)))
-
-(eval-after-load 'ruby-mode
-  '(progn
-     (require 'flymake)
-     (push '(".+\\.rb$" flymake-ruby-init) flymake-allowed-file-name-masks)
-     (push '("Rakefile$" flymake-ruby-init) flymake-allowed-file-name-masks)
-     (push '("^\\(.*\\):\\([0-9]+\\): \\(.*\\)$" 1 2 nil 3)
-           flymake-err-line-patterns)
-     (add-hook 'ruby-mode-hook 'flymake-ruby-enable)))
 (dolist (regex '("\\.rake$" "\\.gemspec$" "\\.ru$" "Rakefile$" "Gemfile$" "Capfile$"))
   (add-to-list 'auto-mode-alist `(,regex . ruby-mode)))
 
