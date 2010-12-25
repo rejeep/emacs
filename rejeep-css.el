e18f24af307eb67e42ef0d33ae129aeb8b4364ab
Johan Andersson <johan.rejeep@gmail.com>
2010-12-25 19:56:44 +0100

    Improve css font-lock regexes.

--- rejeep-css.el
+++ rejeep-css.el
@@ -8,8 +8,7 @@
 (font-lock-add-keywords
  'css-mode
  '(("#[a-fA-F0-9][a-fA-F0-9][a-fA-F0-9]\\([a-fA-F0-9][a-fA-F0-9][a-fA-F0-9]\\)?" . font-lock-reference-face)
-   ("\\([0-9]+\\)\\(?:px\\|em\\|%\\)" 1 font-lock-keyword-face)
-   ("\\(url\\)(" 1 font-lock-function-name-face)))
+   ("\\([0-9]*\\.?[0-9]+\\)\\(?:px\\|em\\|%\\)" 1 font-lock-keyword-face)))
 
 
 (provide 'rejeep-css)
