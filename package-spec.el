f11498a5640d165cf70bd1cc0c8140bc465aad80
Johan Andersson <johan.rejeep@gmail.com>
2011-01-08 14:33:36 +0100

    Install these through ELPA.

---
+++ package-spec.el
@@ -0,0 +1,8 @@
+(source "elpa" "http://tromey.com/elpa/")
+(source "technomancy" "http://repo.technomancy.us/emacs/")
+(source "marmalade" "http://marmalade-repo.org/packages/")
+
+(package "drag-stuff")
+(package "wrap-region")
+(package "ruby-end")
+(package "html-script-src")
