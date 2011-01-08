987256cfda09abf309f020e60331b6f3a3774695
Johan Andersson <johan.rejeep@gmail.com>
2011-01-08 14:23:48 +0100

    Ido is a bad guesser.

--- rejeep-ido.el
+++ rejeep-ido.el
@@ -8,10 +8,8 @@
  ido-enable-prefix nil
  ido-enable-flex-matching t
  ido-create-new-buffer 'always
- ido-use-filename-at-point 'guess
  ido-max-prospects 10
  ido-create-new-buffer 'always
  ido-file-extensions-order '(".rb" ".el"))
-
 
 (provide 'rejeep-ido)
