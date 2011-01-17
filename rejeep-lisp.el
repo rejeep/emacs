f723dec94aefdb6204ba152a5cc72442bf9702e9
Johan Andersson <johan.rejeep@gmail.com>
2011-01-17 19:44:35 +0100

    Color ert definitions.

--- rejeep-lisp.el
+++ rejeep-lisp.el
@@ -1,16 +1,21 @@
 ;;; rejeep-lisp.el --- All things Lisp
 
-
-(add-hook 'emacs-lisp-mode-hook 'turn-on-eldoc-mode)
-
 (require 'highlight-parentheses)
 (add-hook 'find-file-hook 'highlight-parentheses-mode)
+
+(add-hook 'emacs-lisp-mode-hook 'turn-on-eldoc-mode)
+(add-hook 'emacs-lisp-mode-hook
+          (lambda ()
+            (font-lock-add-keywords
+             nil
+             '(("(\\(\\<ert-deftest\\)\\>\\s *\\(\\sw+\\)?"
+                (1 font-lock-keyword-face nil t)
+                (2 font-lock-function-name-face nil t))))))
 
 (define-key lisp-mode-shared-map (kbd "M-&") 'lisp-complete-symbol)
 
 (add-to-list 'interpreter-mode-alist '("emacs" . emacs-lisp-mode))
 
 (put 'ert-deftest 'lisp-indent-function 'defun)
-
 
 (provide 'rejeep-lisp)
