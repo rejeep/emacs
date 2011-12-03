43a01b9bc4aa06371cd5d0b1448c08abd9c0c9d9
Johan Andersson <johan.rejeep@gmail.com>
2011-12-03 15:30:25 +0100

    Use defconst instead of setq.

--- init.el
+++ init.el
@@ -1,9 +1,13 @@
 ;;; init.el - Where it all begins
 
 
-(setq emacs-dir (file-name-directory
-                 (or (buffer-file-name) load-file-name)))
-(setq vendor-dir (expand-file-name "vendor" emacs-dir))
+(defconst emacs-dir
+  (file-name-directory (or (buffer-file-name) load-file-name))
+  "Path to config directory.")
+
+(defconst vendor-dir
+  (expand-file-name "vendor" emacs-dir)
+  "Path to vendor directory.")
 
 ;; Turn off early to avoid momentary display
 (dolist (mode '(menu-bar-mode tool-bar-mode scroll-bar-mode))
