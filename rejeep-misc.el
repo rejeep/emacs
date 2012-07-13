80181eec8faad88a0555643b07d37a57f4945e3a
Johan Andersson <rejeep@Johan-Anderssons-MacBook-Pro.local>
2012-07-13 09:05:16 +0200

    Load Ack and add some shorter aliases.

--- rejeep-misc.el
+++ rejeep-misc.el
@@ -121,4 +121,14 @@
 (require 'popwin)
 (setq display-buffer-function 'popwin:display-buffer)
 
+;; Ack
+(autoload 'ack-and-a-half-same "ack-and-a-half" nil t)
+(autoload 'ack-and-a-half "ack-and-a-half" nil t)
+(autoload 'ack-and-a-half-find-file-same "ack-and-a-half" nil t)
+(autoload 'ack-and-a-half-find-file "ack-and-a-half" nil t)
+(defalias 'ack 'ack-and-a-half)
+(defalias 'ack-same 'ack-and-a-half-same)
+(defalias 'ack-find-file 'ack-and-a-half-find-file)
+(defalias 'ack-find-file-same 'ack-and-a-half-find-file-same)
+
 (provide 'rejeep-misc)
