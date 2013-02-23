97fc6da5ed3f46d19441c7c4be2402fcc88f3e0d
Johan Andersson <johan.rejeep@gmail.com>
2013-02-23 20:42:50 +0100

    Keep window setup when entering magit, q goes back.

--- rejeep-git.el
+++ rejeep-git.el
@@ -3,6 +3,19 @@
 (require 'magit)
 (require 'magit-blame)
 
+(defadvice magit-status (around magit-fullscreen activate)
+  (window-configuration-to-register :magit-fullscreen)
+  ad-do-it
+  (delete-other-windows))
+
+(defun magit-quit-session ()
+  "Restores the previous window configuration and kills the magit buffer"
+  (interactive)
+  (kill-buffer)
+  (jump-to-register :magit-fullscreen))
+
+(define-key magit-status-mode-map (kbd "q") 'magit-quit-session)
+
 (setq magit-default-tracking-name-function 'magit-default-tracking-name-branch-only)
 (setq magit-set-upstream-on-push t)
 
