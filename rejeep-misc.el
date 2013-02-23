f4ed36a430300178e65ab63a9857af06d8719dc4
Johan Andersson <johan.rejeep@gmail.com>
2013-02-23 20:40:51 +0100

    Add multiple cursors.

--- rejeep-misc.el
+++ rejeep-misc.el
@@ -139,4 +139,11 @@
 (require 'smex)
 (smex-initialize)
 
+;; Multiple Cursors
+(require 'multiple-cursors)
+(global-set-key (kbd "C-S-c C-S-c") 'mc/edit-lines)
+(global-set-key (kbd "C->") 'mc/mark-next-like-this)
+(global-set-key (kbd "C-<") 'mc/mark-previous-like-this)
+(global-set-key (kbd "C-c C-<") 'mc/mark-all-like-this)
+
 (provide 'rejeep-misc)
