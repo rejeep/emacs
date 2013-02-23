05eb5433dfb7c984e5f7d5b0c503c43de7e1a174
Johan Andersson <johan.rejeep@gmail.com>
2013-02-23 20:41:39 +0100

    Auto refresh dired.

--- rejeep-misc.el
+++ rejeep-misc.el
@@ -151,4 +151,8 @@
 ;; Auto refresh buffers
 (global-auto-revert-mode 1)
 
+;; Auto refresh dired, but be quiet about it
+(setq global-auto-revert-non-file-buffers t)
+(setq auto-revert-verbose nil)
+
 (provide 'rejeep-misc)
