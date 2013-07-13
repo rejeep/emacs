543077f351fb8cc5ea8fcacdcd1c586b518310f7
Johan Andersson <johan.rejeep@gmail.com>
2013-07-13 20:50:09 +0200

    Add eshell file.

---
+++ rejeep-eshell.el
@@ -0,0 +1,14 @@
+(global-set-key (kbd "M-e") 'eshell)
+
+(add-hook 'eshell-first-time-mode-hook
+          (lambda ()
+            (add-to-list 'eshell-visual-commands "htop")))
+
+(setq eshell-history-size 5000)
+(setq eshell-save-history-on-exit t)
+
+(defun eshell/go-to-root ()
+  (let ((root (find-project-root (eshell/pwd))))
+    (if root (cd root))))
+
+;; (defalias 'r 'eshell/go-to-root)
