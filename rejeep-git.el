695d26d51259027f40dd1d601054d069128f79bf
Johan Andersson <johan.rejeep@gmail.com>
2013-09-03 10:13:24 +0200

    Remove custom magit faces.

--- rejeep-git.el
+++ rejeep-git.el
@@ -19,11 +19,6 @@
 (setq magit-default-tracking-name-function 'magit-default-tracking-name-branch-only)
 (setq magit-set-upstream-on-push t)
 
-(add-hook 'magit-mode-hook
-          (lambda ()
-            (set-face-foreground 'magit-diff-add "green3")
-            (set-face-foreground 'magit-diff-del "red3")))
-
 (setq magit-completing-read-function 'magit-ido-completing-read)
 
 (defun magit-toggle-process-window ()
