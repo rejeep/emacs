d11fe4c2f04e21fdab2af397e90e011c2d6420cf
Johan Andersson <johan.rejeep@gmail.com>
2014-04-01 15:26:31 +0200

    Add binding to local map.

--- init.el
+++ init.el
@@ -263,7 +263,7 @@
   (progn
     (add-hook 'js2-mode-hook (lambda () (setq js2-basic-offset 2)))
     (add-hook 'js2-mode-hook (lambda ()
-                               (bind-key "M-j" 'join-line-or-lines-in-region)))))
+                               (bind-key "M-j" 'join-line-or-lines-in-region js2-mode-map)))))
 
 (use-package coffee-mode
   :config
