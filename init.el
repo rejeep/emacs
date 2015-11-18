e648034586980f98dcd0f6a1164f0596fa9f9866
Johan Andersson <johan.rejeep@gmail.com>
2015-11-18 13:01:50 +0100

    Update Emacs Lisp config

--- init.el
+++ init.el
@@ -295,7 +295,12 @@
       :bind ("C-c e" . macrostep-expand))
     (use-package ert
       :config (add-to-list 'emacs-lisp-mode-hook 'ert--activate-font-lock-keywords)))
-  :bind (("M-." . find-function-at-point))
+  :config
+  (progn
+    (setq tab-always-indent 'complete)
+    (add-to-list 'completion-styles 'initials t))
+  :bind (("M-." . find-function-at-point)
+         ("M-&" . complete-symbol))
   :interpreter (("emacs" . emacs-lisp-mode))
   :mode ("Cask" . emacs-lisp-mode))
 
